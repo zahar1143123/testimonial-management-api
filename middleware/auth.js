0e977ba2ef82d360368a6a308301195418e76f04
zahar1143123 <zaharrubanik2007@gmail.com>
2026-09-08 13:49:35 +0300

    feat: added mongdbB connection logic and jwt auth middlware

---
+++ middleware/auth.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken');
+
+const protect = async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      token = req.headers.authorization.split(' ')[1];
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = decoded;
+
+      return next();
+    } catch (error) {
+      return res.status(401).json({
+        code: 401,
+        status: 'failure',
+        message: 'Не авторизован: недействительный или истекший токен'
+      });
+    }
+  }
+
+  if (!token) {
+    return res.status(401).json({
+      code: 401,
+      status: 'failure',
+      message: 'Не авторизован: токен отсутствует'
+    });
+  }
+};
+
+module.exports = { protect };
